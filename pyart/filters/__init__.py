3b3d07ef53ca02e585ce187135ecabaec782e139
Jordi Figueras <fvj@zueub222.meteoswiss.ch>
2016-08-19 13:52:30 +0000

    changes in clutter ID requested by the Py-ART public library

--- pyart/filters/__init__.py
+++ pyart/filters/__init__.py
@@ -18,6 +18,7 @@ Filtering radar data
 
 """
 
-from .gatefilter import GateFilter, moment_based_gate_filter, moment_based_gate_filter2
+from .gatefilter import GateFilter, moment_based_gate_filter
+from .gatefilter import moment_and_texture_based_gate_filter
 
 __all__ = [s for s in dir() if not s.startswith('_')]
