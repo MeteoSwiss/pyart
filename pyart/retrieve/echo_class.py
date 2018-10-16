bug correction in calculation of entropy

pyart/retrieve/echo_class.py
@@ -422,8 +422,6 @@ def _assign_to_class(zh, zdr, kdp, rhohv, relh, mass_centers,
         weights_mat = np.broadcast_to(
             weights.reshape(nvariables, 1), (nvariables, nbins))
         dist = np.ma.zeros((nclasses, nbins))
-        if t_vals is not None:
-            t_dist_ray = np.ma.zeros((nclasses, nbins))
 
         # compute distance: masked entries will not contribute to the distance
         mask = np.ma.getmaskarray(zh[ray, :])
@@ -436,30 +434,26 @@ def _assign_to_class(zh, zdr, kdp, rhohv, relh, mass_centers,
             dist_ray[mask] = np.ma.masked
             dist[i, :] = dist_ray
 
-            # Transform distances using an exponential transformation
-            if t_vals is None:
-                continue
-
-            t_class = np.array([t_vals[i]])
-            t_class = np.broadcast_to(t_class.reshape(1, 1), (1, nbins))
-
-            t_dist_ray[i, :] = np.ma.exp(-t_class*dist[i, :])
-
-        if t_vals is not None:
-            # set transformed distances to a value between 0 and 1
-            dist_total = np.ma.sum(t_dist_ray, axis=0)
-            dist_total = np.broadcast_to(
-                dist_total.reshape(1, nbins), (nclasses, nbins))
-            t_dist_ray = t_dist_ray/dist_total
-
         # Get hydrometeor class
         class_vec = dist.argsort(axis=0, fill_value=10e40)
         hydroclass_ray = (class_vec[0, :]+1).astype(np.uint8)
         hydroclass_ray[mask] = 0
         hydroclass[ray, :] = hydroclass_ray
 
-        # Compute entropy
         if t_vals is not None:
+            # Transform the distance using the coefficient of the dominant class
+            t_vals_ray = np.ma.masked_where(mask, t_vals[class_vec[0, :]])
+            t_vals_ray = np.broadcast_to(
+                t_vals_ray.reshape(1, nbins), (nclasses, nbins))
+            t_dist_ray = np.ma.exp(-t_vals_ray*dist)
+
+            # set transformed distances to a value between 0 and 1
+            dist_total = np.ma.sum(t_dist_ray, axis=0)
+            dist_total = np.broadcast_to(
+                dist_total.reshape(1, nbins), (nclasses, nbins))
+            t_dist_ray /= dist_total
+
+            # Compute entropy
             entropy_ray = -np.ma.sum(
                 t_dist_ray*np.ma.log(t_dist_ray)/np.ma.log(nclasses), axis=0)
             entropy_ray[mask] = np.ma.masked
@@ -519,8 +513,7 @@ def _assign_to_class_scan(zh, zdr, kdp, rhohv, relh, mass_centers,
     mask = np.ma.getmaskarray(zh)
     dist = np.ma.zeros((nrays, nbins, nclasses))
     t_dist = None
-    if t_vals is not None:
-        t_dist = np.ma.zeros((nrays, nbins, nclasses))
+    entropy = None
     for i in range(nclasses):
         centroids_class = mass_centers[i, :]
         centroids_class = np.broadcast_to(
@@ -531,30 +524,25 @@ def _assign_to_class_scan(zh, zdr, kdp, rhohv, relh, mass_centers,
         dist_aux[mask] = np.ma.masked
         dist[:, :, i] = dist_aux
 
-        if t_vals is None:
-            continue
-
-        t_class = np.array([t_vals[i]])
-        t_class = np.broadcast_to(
-            t_class.reshape(1, 1), (nrays, nbins))
-
-        t_dist[:, :, i] = np.ma.exp(-t_class*dist[:, :, i])
-
-    if t_vals is not None:
-        # set distance to a value between 0 and 1
-        dist_total = np.ma.sum(t_dist, axis=-1)
-        dist_total = np.broadcast_to(
-            dist_total.reshape(nrays, nbins, 1), (nrays, nbins, nclasses))
-        t_dist = t_dist/dist_total
-
     # Get hydrometeor class
     class_vec = dist.argsort(axis=-1, fill_value=10e40)
     hydroclass = (class_vec[:, :, 0]+1).astype(np.uint8)
     hydroclass[mask] = 0
 
-    # compute entroy
-    entropy = None
     if t_vals is not None:
+        # Transform the distance using the coefficient of the dominant class
+        t_vals_aux = np.ma.masked_where(mask, t_vals[class_vec[:, :, 0]])
+        t_vals_aux = np.broadcast_to(
+            t_vals_aux.reshape(nrays, nbins, 1), (nrays, nbins, nclasses))
+        t_dist = np.ma.exp(-t_vals_aux*dist)
+
+        # set distance to a value between 0 and 1
+        dist_total = np.ma.sum(t_dist, axis=-1)
+        dist_total = np.broadcast_to(
+            dist_total.reshape(nrays, nbins, 1), (nrays, nbins, nclasses))
+        t_dist /= dist_total
+
+        # compute entroy
         entropy = -np.ma.sum(
             t_dist*np.ma.log(t_dist)/np.ma.log(nclasses), axis=-1)
         entropy[mask] = np.ma.masked