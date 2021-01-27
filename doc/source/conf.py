98b8aaa2913e9cedbd6e605b101392d62b73377f
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2021-01-27 11:44:59 +0100

    readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,7 @@ import glob
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('../../../../src/pyart/pyart/'))
-
+PYART_RELATIVE_PATH = '../../'
 #import pyart
 
 # -- General configuration ------------------------------------------------
