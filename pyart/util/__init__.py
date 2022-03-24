1395ffdbf408afd6e2747702923cf798af5229db
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2022-03-24 13:31:29 +0100

    readded directional_stats

--- pyart/util/__init__.py
+++ pyart/util/__init__.py
@@ -16,6 +16,7 @@ Direction statistics
 .. autosummary::
     :toctree: generated/
 
+    compute_directional_stats
     angular_mean
     angular_std
     angular_mean_deg
@@ -67,7 +68,7 @@ Miscellaneous functions
 
 """
 
-from .circular_stats import angular_mean, angular_std
+from .circular_stats import compute_directional_stats, angular_mean, angular_std
 from .circular_stats import angular_mean_deg, angular_std_deg
 from .circular_stats import interval_mean, interval_std
 from .circular_stats import mean_of_two_angles, mean_of_two_angles_deg
