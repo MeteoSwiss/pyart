b7914cac20e147934afaed7d79b07d0faae6bfc4
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2023-07-27 15:41:30 +0200

    some adjustments to pyart doc conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -97,6 +97,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Py-ART (MeteoSwiss fork)"
+author = "Jordi Figueras I Ventura, Daniel Wolfensberger"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
